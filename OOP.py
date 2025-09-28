802d1260104c472cf0f3a3bdc83d8c75f202008d
Den_GI <turgunbekovdaniar00@gmail.com>
2025-09-28 13:01:36 +0600

    ControlWork

---
+++ OOP.py
@@ -0,0 +1,111 @@
+# Инкапцуляция
+
+class Person:
+    def __init__(self):
+        self._age = None # это приват артребут
+
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError("Возраст не может быть отрицательным!")
+        self._age = age
+
+    def get_age(self):
+        return self._age
+
+
+p = Person()
+p.set_age(14)
+print(p.get_age())
+
+# p.set_age(-5)  # там выводится ошибка можете проверить
+
+
+# Наследование
+
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "I am an animal"
+
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+
+dog = Dog("buddy")
+cat = Cat("Kitty")
+
+print(dog.name, dog.speak())  # Вывод: Buddy Woof
+print(cat.name, cat.speak())  # Вывод: Kitty Meow
+
+
+# Полиморфизм
+
+
+class Vehicle:
+    def move(self):
+        return "Vehicle is moving"
+
+
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
+
+
+class Bicycle(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+
+
+# Функция полиморфизма
+def move(vehicle):
+    print(vehicle.move())
+
+
+car = Car()
+bike = Bicycle()
+
+move(car)   # Вывод: Car is driving
+move(bike)  # Вывод: Bicycle is pedaling
+
+
+# Абстракция
+
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+rect = Rectangle(10, 5)
+circle = Circle(7)
+
+print(f"Площадь прямоугольника: {rect.area()}") # Вывод: 50
+print(f"Площадь круга: {circle.area()}") # Вывод: ~153.94
